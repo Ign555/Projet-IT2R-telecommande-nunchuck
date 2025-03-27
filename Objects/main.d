fe8f8a29b4e51ab3b3d6a1ed2e62873f075257e2
Ingeno <88973001+Ingeno-pro@users.noreply.github.com>
2025-03-27 19:04:14 +0100

    Few adaptation and bug patching
    HC-05 Delay for not overloading it
    set data format

--- Objects/main.d
+++ Objects/main.d
@@ -1,4 +1,5 @@
 .\objects\main.o: main.c
+.\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\Keil\MDK-Middleware\7.0.0\Board\Board_LED.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 .\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.6.0\CMSIS\Driver\Include\Driver_USART.h
--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "Board_LED.h" 
 #include "Driver_USART.h"   
 #include "Driver_I2C.h"  
@@ -6,6 +7,10 @@
 void callback_UART(){
 	
 	
+}
+#pragma O0
+void wait(int cnt) {
+    while(cnt--);
 }
 void Init_UART(){
 	Driver_USART2.Initialize(callback_UART); //On utilise la fonction de callback définie plus haut
@@ -23,6 +28,7 @@ void Init_UART(){
 int main(){
 	int i;
 	char RxChar, jx=0, jy=0, button=0, data;
+	char buff[8];
 	
 	uint8_t askX[1] = {0x00};
 	uint8_t askY[1] = {0x01};
@@ -41,6 +47,7 @@ int main(){
 	while(1){
 		//Driver_USART2.Receive(&RxChar,1);
 		// à optimiser
+		
 		Driver_I2C1.MasterTransmit (0x52, askX, 1, false); // true = sans stop
 		while (Driver_I2C1.GetStatus().busy == 1); // attente fin transmission
 		Driver_I2C1.MasterReceive (0x52, &jx, 1, false); // false = avec stop
@@ -52,24 +59,18 @@ int main(){
 		while (Driver_I2C1.GetStatus().busy == 1); // attente fin transmission*/
 		
 		//Lecture des boutons
+		
 		Driver_I2C1.MasterTransmit (0x52, askButton, 1, false); // true = sans stop
 		while (Driver_I2C1.GetStatus().busy == 1); // attente fin transmission
 		Driver_I2C1.MasterReceive (0x52, &button, 1, false); // false = avec stop
 		while (Driver_I2C1.GetStatus().busy == 1); // attente fin transmission*/
 		button &= 0x03;
 		
+		//Envoie des trame
+		sprintf(buff, "sx%cy%cb%ce", jx, jy, button);
 		while(Driver_USART2.GetStatus().tx_busy == 1);
-		Driver_USART2.Send("x",1); // send the read characters
-		while(Driver_USART2.GetStatus().tx_busy == 1);
-		Driver_USART2.Send(&jx,1); // send the read characters
-		while(Driver_USART2.GetStatus().tx_busy == 1);
-		Driver_USART2.Send("y",1); // send the read characters
-		while(Driver_USART2.GetStatus().tx_busy == 1);
-		Driver_USART2.Send(&jy,1); // send the read characters
-		while(Driver_USART2.GetStatus().tx_busy == 1);
-		Driver_USART2.Send("b",1); // send the read characters
-		while(Driver_USART2.GetStatus().tx_busy == 1);
-		Driver_USART2.Send(&button,1); // send the read characters
+		Driver_USART2.Send(buff,8); // send the read characters
+	  wait(100000); //Délai pour ne pas surcharger le HC-05
 
 	}
 }
